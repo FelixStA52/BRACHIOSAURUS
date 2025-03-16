beefe0f466d74c8df7bb0de8a4ec28dc3f86688d
Félix St-Amour <felix.st-amour@mail.mcgill.ca>
2025-03-16 14:46:16 -0400

    Add files via upload

---
+++ Code/Interactive Config/config.h
@@ -0,0 +1,21 @@
+// config.h
+
+#define BOX_NUMBER "44" //number of the box
+
+#define SAMPLES 2048 //number of samples taken, needs to be a power of 2 i.e. 2^n
+
+#define EPOCH_TIME 1700000000//1741966595 //Unix time corresponding to when the BRACHIOSAURUS is turned on
+
+#define LOOPS_PER_WAKE_UP 2 //how many data points are taken in a wake up loop
+
+#define SEC_TO_SLEEP 60//14400 //time (seconds) the microcontroller goes to sleep between taking data
+
+#define DEPLOYMENT_START 1745144060//1754020860 //reasonable satrt time for when the data can be recovered, set to August 1rst
+
+#define DEPLOYMENT_END 1755144060 //reasonable end time for when the data can be recovered, set to August 14th
+
+#define DEPLOYMENT_WAIT 600 //max number of seconds to wait for wifi option
+
+#define REMAINING_WIFI_ACCESS_NUMBER 2 //number of times the wifi can be accessed
+
+//set DEPLOYMENT_START and DEPLOYMENT_END to 0 if you do not plan on using the wifi option
