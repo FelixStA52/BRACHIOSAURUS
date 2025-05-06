afe213e5e3d0d31adffb997d310cfcf7f855d169
Félix St-Amour <felix.st-amour@mail.mcgill.ca>
2025-05-06 00:55:50 -0400

    Update config.h

--- ESP32_code/config.h
+++ ESP32_code/config.h
@@ -4,11 +4,11 @@
 
 #define SAMPLES 2048 //number of samples taken, needs to be a power of 2 i.e. 2^n
 
-#define EPOCH_TIME 0//1742167556 //Unix time corresponding to when the BRACHIOSAURUS is turned on
+#define EPOCH_TIME 1742167556 //Unix time corresponding to when the BRACHIOSAURUS is turned on
 
-#define LOOPS_PER_WAKE_UP 2//10 //how many data points are taken in a wake up loop
+#define LOOPS_PER_WAKE_UP 10 //how many data points are taken in a wake up loop
 
-#define SEC_TO_SLEEP 20//14400 //time (seconds) the microcontroller goes to sleep between taking data
+#define SEC_TO_SLEEP 14400 //time (seconds) the microcontroller goes to sleep between taking data
 
 #define DEPLOYMENT_START 1754020860 //reasonable satrt time for when the data can be recovered, set to August 1rst
 
